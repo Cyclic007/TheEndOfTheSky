543e111ee6821ea12bee2b77eec74baced6e582f
Xnightslayer99 <68559143+Xnightslayer99@users.noreply.github.com>
2022-02-18 14:09:21 +0000

    Made it actually runnable

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def fight(number):
     enemy3_hp = 10
     enemy1_damage = 1
     print('')
-    if enemy1_hp =< 0 and enemy2_hp =< 0 enemy3_hp =< 0:
+    if enemy1_hp <= 0 and enemy2_hp <= 0 and enemy3_hp <= 0:
       print('')
   elif number == 2:
     enemy1_hp = 20
@@ -30,10 +30,11 @@ def fight(number):
     print('Error!')
 def commands(command):
   if command == 'exit':
+    exit()
 print('Bartender- Welcome to my tavern! You must have come a long way.')
 sleep(1)
 one = input(
-    'Bartender- How about a drink? We have water and milk! 1-water 2-milk')
+    'Bartender- How about a drink? We have water and milk! 1-water 2-milk: ')
 if one == '1':
     print('Bartender- Ok! One water coming right up!')
 elif one == '2':
@@ -45,13 +46,13 @@ print('As you look around, you see a mysterous person in the background.')
 print('He looks at you and walks over.')
 sleep(1)
 two = input(
-    'Person- I have not seen you around here. Are you new to these parts?')
+    'Person- I have not seen you around here. Are you new to these parts? ')
 print(
     'Person- Well, if your new or not, I do not like attention. Stay off my back.'
 )
 sleep(1)
 print('He walks back to his table.')
-three = input('Do you follow him? y/n')
+three = input('Do you follow him? y/n: ')
 if three == 'y':
     print('You jump behind a table and creep closer.')
 else:
@@ -65,10 +66,10 @@ print('The bandits walk in and shove over the patrens.')
 sleep(1)
 print('They walk right past you, pockets filled with gold.')
 sleep(1)
-four = input('Do you attack? y/n')
+four = input('Do you attack? y/n: ')
 if four == 'y':
   print()
 elif four == 'n':
   print()
 else:
-  print('Please follow instructions. You will attack. Happy now?')
+  print('Please follow instructions. You will attack. Happy now? ')
